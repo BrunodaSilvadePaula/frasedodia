c92034d9010b2456e150d613a52d859b8b062709
Bruno <bruno@bettacom.com.br>
2017-05-10 20:31:01 -0300

    Add layout changes and change in method create
    Alter background for project, add change in create method to print an
    array value.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Frase do dia #
+
+It is a simple project used to learn how to work with array in an android project
+
--- app/src/main/java/frasedodia/cursoandroid/com/frasedodia/MainActivity.java
+++ app/src/main/java/frasedodia/cursoandroid/com/frasedodia/MainActivity.java
@@ -2,12 +2,24 @@ package frasedodia.cursoandroid.com.frasedodia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textNewPhase;
+    private Button btnNewPhrase;
+    private String[] phrase = {"teste", "teste2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textNewPhase = (TextView) findViewById(R.id.textNewPhrase);
+        btnNewPhrase = (Button) findViewById(R.id.btnNewPhrase);
+
+        textNewPhase.setText(phrase[1]);
+
     }
 }
